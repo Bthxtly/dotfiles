79b57c247eaca27f8bd941ce52c7ad63992c7c78
Bthxtly <bthxtly@gmail.com>
2025-11-24 14:35:31 +0800

    fish: add fallback for fzf preview(using pygmentize)

--- dot_config/fish/functions/fzf.fish
+++ dot_config/fish/functions/fzf.fish
@@ -2,8 +2,8 @@ function fzf --wraps="fzf"
     # Paste contents of preferred variant here
 
     command fzf --bind=ctrl-e:preview-down,ctrl-y:preview-up \
-                --reverse \
-                --color=16 \
-                --preview 'pygmentize -f terminal {}' \
-                $argv
+        --reverse \
+        --color=16 \
+        --preview 'pygmentize -f terminal -g {}' \
+        $argv
 end
