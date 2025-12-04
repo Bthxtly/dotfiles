180b48802efa66ea4b58dd80884cd6a7032f3467
Bthxtly <bthxtly@gmail.com>
2025-12-04 16:06:02 +0800

    nvim: more sensible vim.o.textwidth

--- dot_config/nvim/ftplugin/typst.lua
+++ dot_config/nvim/ftplugin/typst.lua
@@ -1,5 +1,5 @@
 vim.bo.commentstring = "// %s"
-vim.bo.textwidth = 79
+vim.bo.textwidth = 99
 vim.o.colorcolumn = "+1"
 vim.o.spell = true
 vim.o.scrolloff = 5
--- dot_config/nvim/ftplugin/vimwiki.lua
+++ dot_config/nvim/ftplugin/vimwiki.lua
@@ -1,4 +1,4 @@
 vim.o.spell = true
 vim.o.wrap = false
-vim.o.textwidth = 79
+vim.o.textwidth = 99
 vim.o.colorcolumn = "+1"
